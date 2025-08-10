195b25165eb3d8d39ee1b3a426488648a4e1dc4e
Sota20000121 <ntv-350-pvg@e-net.nara.jp>
2025-08-10 15:31:10 +0000

    8_10

--- move.js
+++ move.js
@@ -1,6 +1,8 @@
+//main画像
 window.onload = function() {
     const fade = document.getElementsByClassName('img_move');
 fade[0].classList.add('show');
+document.querySelector('main').classList.add('show');
 }
 
 $(window).scroll(function(){
@@ -15,4 +17,40 @@ $(window).scroll(function(){
     });
 });
 
+//ハンバーガー
+(function($){
+    var $nav = $('#navArea');
+    var $btn = $('.toggle_btn');
+    var $mask = $(`#mask`);
+    var open = 'open';
 
+    $btn.on('click',function(){
+        if( ! $nav.hasClass(open)){
+            $nav.addClass(open);
+        }else{
+            $nav.removeClass(open);
+        }
+    });
+
+    $mask.on(`click`,function(){
+        $nav.removeClass(open);
+    });
+})(jQuery);
+
+//メインのフェードイン
+const target = document.querySelectorAll('.fade-slide');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        }
+    });
+},{
+    threshold: 0.1
+});
+
+target.forEach(target => {
+    observer.observe(target);
+});
